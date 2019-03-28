2d88e8894e54ee2faa1df5a8ddb4abac82182bf3
MD Riaz <riazmd582@gmail.com>
2019-03-29 00:04:31 +0600

    remoed unnecessary js & css... modified and re desined withn simple code.

--- upload.php
+++ upload.php
@@ -1,26 +1,38 @@
 <?php
 //This line assigns a random number to a variable. 
 $ran = rand () ;
- //This takes the random number (or timestamp) you generated and adds a __ on the end, so it is ready for the file extension to be appended.
+
+//This takes the random number (or timestamp) you generated and adds a __ on the end, so it is ready for the file extension to be appended.
 $ran2 = $ran."___";
 
-$source_path = $_FILES['uploadFile']['tmp_name'];
-$target_path = 'upload/' .$ran2.$_FILES['uploadFile']['name'];
+//set source path to a variable.
+$source_path = $_FILES['fileToUpload']['tmp_name'];
 
-//Now starting upload
-if(!empty($_FILES))
-{
+//combine file name with random number & set file directory.
+$target_file = 'upload/' .$ran2.$_FILES['fileToUpload']['name'];
+$uploadOk = 1; //if condition is not fullfilled set this to 0.
+if($_FILES['fileToUpload']['tmp_name'] == "") {
+    $uploadOk = 0;
+    }else{
+        $uploadOk = 1;
+    }
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, there was an error uploading your file.";
+} 
+// if everything is ok, try to upload file
+else{
 	if(is_uploaded_file($source_path))
-	{
-		sleep(1);
-		if(move_uploaded_file($source_path, $target_path))
-		{
-			echo '<img src="'.$target_path.'" class="img-thumbnail" width="300" height="250" /><br>';
-            echo "The File Successfully Uploaded as " .$ran2.$_FILES['uploadFile']['name'];
-		}
-	}
-}
-
+        {
+            sleep(1);
+            if(move_uploaded_file($source_path, $target_file))
+            {
+                echo '<img src="'.$target_file.'" class="img-thumbnail" width="300" height="250" /><br>';
+                echo "The File Successfully Uploaded as <br> " .$ran2.$_FILES['fileToUpload']['name'];
+                echo '<br> <a href="http://riaz.ml" style="text-decoration: none;font-family: Poppins, sans-serif;background: #FFEB3B;">Go To Home</a>'; 
+            }
+        }
+    }
 // set your e-mail address first, where you'll receive the notifications
 $yourEmailAddress = "bpimdriaz@gmail.com";
 
@@ -30,5 +42,4 @@ $emailContent = "Someone visited your webpage. IP address:".$remoteIpAddress;
 
 // send the message
 mail($yourEmailAddress, $emailSubject, $emailContent);
-
 ?>
