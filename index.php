664f2a317f83c17744ed895b2da402a3f432e293
MD Riaz <riazmd582@gmail.com>
2019-04-01 20:15:37 +0600

    added meta info

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 <head>
 	<title>File Upload</title>
 	<meta charset="UTF-8">
+    <meta name="description" content="Upload files with AJAX">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+    <meta name="author" content="MD Riaz">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="favicon.png" rel="icon" type="image/x-icon" />
 	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
